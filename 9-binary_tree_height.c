074cb89728814718a793cddb71edb584cac416c7
vagava <vagava.123@gmail.com>
2021-07-21 17:55:45 -0500

    fixed betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (-1);
-	else
-	{
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
 
-		if (left > right)
-			return (left + 1);
-		else
-			return (right + 1);
-	}
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
 }
