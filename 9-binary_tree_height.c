517713451f25559db1a6a008a9170080037aee42
vagava <vagava.123@gmail.com>
2021-07-21 18:49:46 -0500

    add conditions tree>left && tree>right to return 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
 
-	if (!tree)
-		return (-1);
-
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left > right)
