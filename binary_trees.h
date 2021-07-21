ba671027da4979eab409b637e7bf7c22921fc42a
Felipe Prado <jfpc11@misena.edu.co>
2021-07-20 21:46:30 -0500

    adding the print prototype

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 #include <stdio.h>
+#include <stddef.h>
+
 /*Structs*/
 /**
  * struct binary_tree_s - Binary tree node
@@ -19,6 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/*Print prototype*/
+void binary_tree_print(const binary_tree_t *);
 
 /*Prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
