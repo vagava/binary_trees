599cabf168e2f9f2766cbc23229eb44eb1438e3e
vagava <vagava.123@gmail.com>
2021-07-21 20:42:45 -0500

    add 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,17 +6,13 @@
  * 
  * 
  */
-size_t _height(const binary_tree_t *tree);
+size_t altura(const binary_tree_t *tree);
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
 	if (!tree)
 		return (0);
-
-	left = _height(tree->left);
-	right = _height(tree->right);
-	return (left - right);
+	return ((altura(tree->left)) - (altura(tree->right)));
 }
 
 /**
@@ -24,16 +20,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 * @tree: is a pointer to the root node of the tree to measure the height.
 * Return: rhe size of tree
 */
-size_t _height(const binary_tree_t *tree)
+size_t altura(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	size_t left_height;
+	size_t right_height;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (tree == NULL)
 		return (0);
-	left = _height(tree->left);
-	right = _height(tree->right);
-	if (left > right)
-		return (left + 1);
+
+	if (tree->left)
+		left_height = altura(tree->left) + 1;
 	else
-		return (right + 1);
+		left_height = 1;
+	if (tree->right)
+		right_height = altura(tree->right) + 1;
+	else
+		right_height = 1;
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
